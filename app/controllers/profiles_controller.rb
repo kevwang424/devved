7b6912806cc9b09ab2344933637c968dd530a2dd
Kevin Wang <kev.wang424@gmail.com>
2017-03-27 19:26:39 -0400

    created the profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,26 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
 
+  #POST to users/:user_id/profile
+  def create
+    #Ensure we have the user that is filling out the form/signed in
+    @user = User.find(params[:user_id])
+    #Create profile that is linked to this specific user
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Your profile has been successfully updated"
+      redirect_to root_path
+    else
+      #if it does not save, we will render the form again
+      render action: :new
+    end
+  end
+
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
+
 end
