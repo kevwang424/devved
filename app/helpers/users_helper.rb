7035b8e1d615bf1b809225fdb3af36db198b5854
Kevin Wang <kev.wang424@gmail.com>
2017-03-27 22:17:00 -0400

    added icons to different job titles

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+  def job_title_icon
+    if @user.profile.job_title == "Developer"
+      "<i class='fa fa-code'></i>".html_safe
+    else
+      "<i class='fa fa-cog'></i>".html_safe
+    end
+  end
+end
