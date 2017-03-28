260e25f1e920a90db4627bea4de6cac72a3355db
Kevin Wang <kev.wang424@gmail.com>
2017-03-27 20:23:56 -0400

    added barebones user show page and updated profile redirect

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,11 +8,12 @@ class ProfilesController < ApplicationController
   def create
     #Ensure we have the user that is filling out the form/signed in
     @user = User.find(params[:user_id])
-    #Create profile that is linked to this specific user
+    #Create profile that is linked to this specific user. Use #build when we have association, it is
+    #better than using #new in this case.
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Your profile has been successfully updated"
-      redirect_to root_path
+      redirect_to user_path(params[:user_id])
     else
       #if it does not save, we will render the form again
       render action: :new
